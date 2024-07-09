836c77a8a5b0fa58f65647bb8438562b12804fef
Benjamin Lee <benjaminlee755@gmail.com>
2024-07-09 01:31:50 -0400

    made filter and searchbar sticky

--- js/search.js
+++ js/search.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const restaurants = [
         { name: 'Restaurant 1', distance: '1km', cuisine: 'Italian', rating: '5', price: '$$', opening: 'Morning', delivery: 'Yes' },
         { name: 'Restaurant 2', distance: '5km', cuisine: 'Chinese', rating: '4', price: '$', opening: 'Evening', delivery: 'No' },
-        { name: 'Restaurant 3', distance: '10km', cuisine: 'Indian', rating: '3', price: '$$$', opening: 'Afternoon', delivery: 'Yes' },
+        { name: 'Restaurant 3', distance: '10km', cuisine: 'Indian', rating: '2', price: '$$$', opening: 'Afternoon', delivery: 'Yes' },
         { name: 'Restaurant 4', distance: '1km', cuisine: 'Italian', rating: '5', price: '$$$$', opening: 'Morning', delivery: 'Yes' },
         { name: 'Restaurant 5', distance: '5km', cuisine: 'Chinese', rating: '4', price: '$', opening: 'Evening', delivery: 'No' },
         { name: 'Restaurant 6', distance: '1km', cuisine: 'Chinese', rating: '3', price: '$$', opening: 'Afternoon', delivery: 'No' },
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
         { name: 'Restaurant 8', distance: '5km', cuisine: 'Chinese', rating: '4', price: '$', opening: 'Afternoon', delivery: 'No' },
         { name: 'Restaurant 9', distance: '10km', cuisine: 'Indian', rating: '5', price: '$$$$', opening: 'Afternoon', delivery: 'Yes' },
         { name: 'Restaurant 10', distance: '10km', cuisine: 'Indian', rating: '5', price: '$$', opening: 'Morning', delivery: 'No' },
+        { name: 'Restaurant 11', distance: '20km', cuisine: 'Italian', rating: '1', price: '$$$', opening: 'Evening', delivery: 'No' },
         { name: 'Restaurant 12', distance: '5km', cuisine: 'Chinese', rating: '4', price: '$', opening: 'Afternoon', delivery: 'No' },
         { name: 'Restaurant 13', distance: '10km', cuisine: 'Italian', rating: '3', price: '$$$', opening: 'Afternoon', delivery: 'Yes' },
     ];
