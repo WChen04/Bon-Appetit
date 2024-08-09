2cf1392324ff3444a82d81188bfc279d2d9301e9
sonickels <96350461+sonickels@users.noreply.github.com>
2024-08-09 19:57:13 -0400

    Combined queries.js and server.js

--- js/server.js
+++ js/server.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-const db = require('./queries')
+//const db = require('./queries')
 
 const app = express();
 const port = 3000;
 
 // Use CORS middleware to allow requests from your frontend
 app.use(cors());
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'softwaredd',
+  port: 5432,
+})
+
+const getRestaurants = (request, response) => {
+
+  pool.query('SELECT id, name, rating, latitude, longitude, price FROM restaurants', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
 
 // Replace 'YOUR_API_KEY' with your actual Yelp API key
 const API_KEY =
@@ -34,7 +53,7 @@ app.get("/api/yelp", async (req, res) => {
   }
 });
 
-app.get('/', db.getRestaurants)
+app.get('/', getRestaurants)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
